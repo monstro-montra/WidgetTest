5ab8166fbd39328f4aaaa0094adb68b41235d33e
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2023-02-23 22:42:30 -0500

    learning how to make widgets in android!

---
+++ app/src/main/java/com/javierlabs/WidgetTest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.javierlabs.WidgetTest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
